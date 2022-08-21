828c9e7d305162988cd38098ef5bae61a89fb254
Marinadecampos12 <marina.campospilatti03@gmail.com>
2022-08-21 20:57:52 -0300

    first commit

---
+++ array-challenge.java
@@ -0,0 +1,50 @@
+import java.util.*; 
+import java.io.*;
+
+class Main {
+
+  public static String ArrayChallenge(String[] strArr) {
+    // code goes here
+      String palavra1 = strArr[0];
+      String dicio = strArr[1];
+      String[] palasepar = palavra1.split("");
+      String[] diciosepar = dicio.split(",");
+      List<String> list1 = Arrays.asList(diciosepar);
+      String paljun = "";
+      String paljun2 = "";
+      
+      
+      for(int x = 4; x<palasepar.length;x++){
+          if(!list1.contains(paljun2)){
+            paljun2 = paljun2 + palasepar[x];
+          }else{
+            paljun2 = paljun2 + palasepar[x];
+            if(x==palasepar.length){
+              return paljun2;
+            }
+            }
+      };
+
+      for(int i = 0; i<palasepar.length;i++){
+          if(!list1.contains(paljun)){
+            paljun = paljun + palasepar[i];
+          }else{
+            paljun = paljun + palasepar[i];
+            if(i>2 && i < 4){
+              return  new StringBuilder("kpaqrfi23c:" + paljun + "," + paljun2).reverse().toString();
+            }
+            }
+      };
+
+      
+      return paljun;
+      };
+      
+  
+ public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(ArrayChallenge(s.nextLine())); 
+  }
+
+}
